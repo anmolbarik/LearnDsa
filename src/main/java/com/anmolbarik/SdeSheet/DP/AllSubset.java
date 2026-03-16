ba2c9c62036881e55c7b7a90344616d8583b4b39
Anmol Barik <30435955+anmolbarik@users.noreply.github.com>
2026-03-16 17:13:32 +0530

    recursion and backtracking problems

---
+++ src/main/java/com/anmolbarik/SdeSheet/DP/AllSubset.java
@@ -0,0 +1,29 @@
+package com.anmolbarik.SdeSheet.DP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllSubset {
+
+    public static  void helper(int index, List<Integer> current, int[] arr, List<List<Integer>> result){
+        if(index == arr.length){
+            result.add(new ArrayList<>(current));
+            //System.out.println(result);
+            return;
+        }
+        current.add(arr[index]);
+        helper(index+1, current, arr,result);
+        current.remove(current.size()-1);
+        helper(index+1, current, arr,result);
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        List<List<Integer>> result = new ArrayList<>();
+         helper(0,new ArrayList<>(), arr, result);
+        System.out.println(result);
+
+    }
+}
---
+++ src/main/java/com/anmolbarik/SdeSheet/DP/coinChange2.java
@@ -0,0 +1,25 @@
+package com.anmolbarik.SdeSheet.DP;
+
+public class coinChange2 {
+
+    public static int getMaximumways(int amount, int[] coins){
+
+        int[] dp = new int[amount+1];
+
+        dp[0] = 1;
+
+        for(int coin: coins){
+            for(int i=coin; i<=amount; i++){
+                dp[i] = dp[i] + dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1,2,5};
+        int amount = 2;
+
+        System.out.println("Answer: "+getMaximumways(amount,coins));
+    }
+}

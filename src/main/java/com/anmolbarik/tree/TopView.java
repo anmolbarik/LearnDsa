003240444020852dd211ce445c1b388415751be8
Anmol Barik <anmolbarikab@gmail.com>
2026-03-20 17:59:01 +0530

    tree top view left view

---
+++ src/main/java/com/anmolbarik/tree/TopView.java
@@ -0,0 +1,15 @@
+package com.anmolbarik.tree;
+
+
+
+public class TopView {
+   public void test(){
+
+   }
+
+    public static void main(String[] args) {
+       Node root = new Node(50);
+
+        return;
+    }
+}

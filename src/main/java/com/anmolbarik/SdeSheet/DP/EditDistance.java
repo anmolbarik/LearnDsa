1b76c2a8cfbeab2c284ed0ef997ed138fdcc8aac
Anmol Barik <30435955+anmolbarik@users.noreply.github.com>
2026-03-22 16:27:16 +0530

    recursion and backtracking problems

---
+++ src/main/java/com/anmolbarik/SdeSheet/DP/EditDistance.java
@@ -0,0 +1,45 @@
+package com.anmolbarik.SdeSheet.DP;
+
+//Steps to make another String
+
+public class EditDistance {
+
+    public static int editDistance(String a, String b){
+        int[][] dp  = new int[a.length()+1][b.length()+1];
+
+        //base condition
+        for(int i =0; i<=a.length(); i++){
+            dp[i][0] = i;
+
+
+        }
+        for(int i =0; i<=b.length(); i++){
+            dp[0][i] = i;
+        }
+
+        for(int i =1; i<=a.length(); i++){
+            for(int j=1; j<=b.length(); j++){
+
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]),dp[i-1][j-1]) + 1;
+                }
+            }
+        }
+
+        for(int i =0; i<=a.length(); i++){
+            for(int j=0; j<=b.length(); j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println(" ");
+        }
+        return dp[a.length()][b.length()];
+    }
+
+    public static void main(String[] args) {
+        String a = "abcab";
+        String b = "abd";
+        System.out.println("Edit "+editDistance(a,b));
+    }
+}

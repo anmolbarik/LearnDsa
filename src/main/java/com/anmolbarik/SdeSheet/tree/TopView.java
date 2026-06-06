884aaac467434c84d3bac2670761f813f713396b
Anmol Barik <30435955+anmolbarik@users.noreply.github.com>
2026-06-06 17:00:36 +0530

    top view

--- src/main/java/com/anmolbarik/SdeSheet/tree/TopView.java
+++ src/main/java/com/anmolbarik/SdeSheet/tree/TopView.java
@@ -1,15 +1,50 @@
 package com.anmolbarik.SdeSheet.tree;
 
+import java.util.Map;
+import java.util.TreeMap;
 
+class Node{
+    Node left, right;
+    int data;
+
+    Node(int i){
+        data=i;
+    }
+}
 
 public class TopView {
-   public void test(){
 
-   }
+    public static void printTopView(Node root, int level, Map<Integer,Integer> map){
+        if(root==null){
+            return;
+        }
+        if(!map.containsKey(level)){
+            map.put(level, root.data);
+        }
+
+        printTopView(root.left,level-1, map);
+        printTopView(root.right,level+1, map);
+    }
 
     public static void main(String[] args) {
-       Node root = new Node(50);
+        Map<Integer,Integer> map = new TreeMap<>();
+         /*Populating demo BST
+                1
+              /   \
+             2     3
+             \    / \
+              8  5   6
+        */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.right = new Node(8);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        printTopView(root,1,map);
 
-        return;
+        for(int i:map.values()){
+            System.out.println(i);
+        }
     }
 }
